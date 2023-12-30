e5d53bcdb1061018e5b5bb1db71628b36f57670e
hansanaCham <hansanacham@gmail.com>
2023-12-30 21:06:28 +0530

    finish the project

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-const a = 5
-const b = 7
-console.log(a + b)
+const http = require("http");
+const fs = require("fs");
 
-const fs = require('fs');
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  // console.log(url,method);
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title> Input form </title></head>");
+    res.write("<body>");
+    res.write(
+      '<form action = "/message" method = "POST"> <input type = "text" name="message"> <button type = "submit"> Submit </button></form>'
+    );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
 
-fs.writeFile('abc.txt', 'Hello, this is the content of the file.', (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-    } else {
-      console.log('File written successfully.');
-    }
-  });
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        if (err != null) {
+          console.log("Error In writing a file");
+        }
+      });
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+});
+
+server.listen(3000);
